7b8812cfc1e4e540f5a90c88974050af8abf221a
Nahom.T <ntade499@gmail.com>
2024-10-15 04:19:13 +0300

    Update axios.jsx

--- src/Api/axios.jsx
+++ src/Api/axios.jsx
@@ -4,7 +4,7 @@ const axiosInstance = axios.create({
   //Local instance of firebase functions
   //   baseURL: "http://127.0.0.1:5001/clone-58df5/us-central1/api",
   // deployed version of amazon server on render.com
-  baseURL: "https:amazon-api-deploy-hj9z.onrender.com",
+  baseURL: "https://amazon-api-deploy-1-dc1b.onrender.com,
 });
 
 export { axiosInstance };
